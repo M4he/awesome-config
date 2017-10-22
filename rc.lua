7277f1614a0d96d85301fa8d4ba2f47a3613546d
M4he <moekoi@posteo.jp>
2017-10-22 22:31:07 +0200

    rc.lua: comment problematic workaround code

--- rc.lua
+++ rc.lua
@@ -8,10 +8,10 @@ require(rc)
 
 -- workaround for unmaximized clients missing their border
 -- see: https://github.com/awesomeWM/awesome/issues/1692
-local beautiful = require("beautiful")
-client.connect_signal("request::geometry", function(c)
-    if client.focus then
-        client.focus.ignore_border_width = false
-        client.focus.border_width = beautiful.border_width or 1
-    end
-end)
+-- local beautiful = require("beautiful")
+-- client.connect_signal("request::geometry", function(c)
+--     if client.focus then
+--         client.focus.ignore_border_width = false
+--         client.focus.border_width = beautiful.border_width or 1
+--     end
+-- end)
