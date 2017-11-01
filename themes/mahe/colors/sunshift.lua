dcaa3d734b12dbac3712a1d2d1ed8118563d8c19
M4he <moekoi@posteo.jp>
2017-11-01 20:11:02 +0100

    sunshift: experimental color update

--- themes/mahe/colors/sunshift.lua
+++ themes/mahe/colors/sunshift.lua
@@ -5,15 +5,15 @@ theme.color = {
     gray      = "#73807F",
     bg        = "#25292E",
     bg_second = "#292D33",
-    wibox     = "#2D343F",
+    wibox     = "#282F38",
     icon      = "#EEEEEE",
     text      = "#EEEEEE",
     urgent    = "#FF4070",
-    highlight = "#2D343F",
+    highlight = "#282F38",
 
     border    = "#404040",
     shadow1   = "#1B1C1F",
-    shadow2   = "#39414D",
+    shadow2   = "#353C47",
     shadow3   = "#14171C",
     shadow4   = "#546175",
 
