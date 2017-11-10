fe14226cc8be4b5a0ba3dddc46ff698a8c6fa02e
M4he <moekoi@posteo.jp>
2017-11-10 16:16:47 +0100

    make left and right edges switch tag on click

--- mahe/edges-config.lua
+++ mahe/edges-config.lua
@@ -35,8 +35,8 @@ function edges:init(args)
 	right.wibox:geometry(egeometry["right"])
 
 	right.layout:buttons(awful.util.table.join(
-		awful.button({}, 5, function() awful.tag.viewnext(mouse.screen) end),
-		awful.button({}, 4, function() awful.tag.viewprev(mouse.screen) end)
+		awful.button({}, 1, function() awful.tag.viewnext(mouse.screen) end),
+		awful.button({}, 3, function() awful.tag.viewprev(mouse.screen) end)
 	))
 
 	-- Left
@@ -44,18 +44,10 @@ function edges:init(args)
 	local left = redflat.util.desktop.edge("vertical", { ew, workarea.height - ew })
 	left.wibox:geometry(egeometry["left"])
 
-	left.area[1]:buttons(awful.util.table.join(
-		awful.button({}, 4, function() switcher:show({ filter = allscreen })                 end),
-		awful.button({}, 5, function() switcher:show({ filter = allscreen, reverse = true }) end)
+	left.layout:buttons(awful.util.table.join(
+		awful.button({}, 1, function() awful.tag.viewnext(mouse.screen) end),
+		awful.button({}, 3, function() awful.tag.viewprev(mouse.screen) end)
 	))
-
-	left.area[2]:buttons(awful.util.table.join(
-		awful.button({}, 9, function() if client.focus then client.focus.minimized = true end  end),
-		awful.button({}, 4, function() switcher:show({ filter = currenttags })                 end),
-		awful.button({}, 5, function() switcher:show({ filter = currenttags, reverse = true }) end)
-	))
-
-	left.wibox:connect_signal("mouse::leave", function() redflat.float.appswitcher:hide() end)
 end
 
 -- End
