c7229bf4b3d86d47179f28181f50d6e266fdc944
M4he <moekoi@posteo.jp>
2018-02-08 10:33:14 +0100

    add customized ercheck-config

--- mahe/ercheck-config.lua
+++ mahe/ercheck-config.lua
@@ -1,1 +1,27 @@
-../colorless/ercheck-config.lua
+local naughty = require("naughty")
+
+if awesome.startup_errors then
+	naughty.notify({
+		preset = naughty.config.presets.critical,
+		title  = "WINDOW MANAGER STARTUP ERROR",
+		text   = awesome.startup_error
+	})
+end
+
+do
+	local in_error = false
+	awesome.connect_signal(
+		"debug::error",
+		function (err)
+			if in_error then return end
+			in_error = true
+
+			naughty.notify({
+				preset  = naughty.config.presets.critical,
+				title   = "WINDOW MANAGER ERROR",
+				text    = err
+			})
+			in_error = false
+		end
+	)
+end
