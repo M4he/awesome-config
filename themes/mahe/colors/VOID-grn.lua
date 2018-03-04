7851c9f039a193e64275a45e134ab07d59c6da90
M4he <moekoi@posteo.jp>
2018-03-04 20:36:22 +0100

    theme: add VOID color schemes

---
+++ themes/mahe/colors/VOID-grn.lua
@@ -0,0 +1,24 @@
+local theme = {}
+
+theme.color = {
+    main      = "#8BC34A",  -- main accent
+    gray      = "#737373",  -- used for inactive elements and bar trunks
+    bg        = "#292929",  -- bg used for custom widgets (e.g. appswitcher, top)
+    bg_second = "#1F1F1F",  -- alternating lines for 'bg'
+    wibox     = "#333333",  -- border, panel and general background color
+    icon      = "#EEEEEE",  -- icons in menus
+    text      = "#EEEEEE",  -- text in menus and titlebars
+    urgent    = "#FF4070",  -- urgent window highlight in taglist, tasklist also volume mute
+    highlight = "#333333",  -- text when highlighted in menu
+    empty     = "#4D4D4D",  -- circle tag empty color
+
+    border    = "#404040",  -- tooltip border
+    shadow1   = "#404040",  -- separator dark side
+    shadow2   = "#404040",  -- separator bright side
+    shadow3   = "#737373",  -- buttons outer border
+    shadow4   = "#737373",  -- buttons inner border
+
+    secondary = "#404040"
+}
+
+return theme
